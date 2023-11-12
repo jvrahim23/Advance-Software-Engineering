098a984609c9971cebeed6ae525de649a586541f
jwilson23 <jvrahim23@yahoo.com>
2023-11-12 17:03:45 +0100

    Day 4 files

---
+++ Week 2/nov12.js
@@ -0,0 +1,16 @@
+alert("Welcome");
+console.log('2+2');
+console.log(2+2);
+
+// values
+// data types: number, strings, boolean, null, undefined
+// variable
+
+const fistStu='Peter'
+
+let newStuNew='Lucas'
+newStuNew=newStuNew+' '+'Peter'
+console.log(newStuNew);
+newStuNew='Jack';
+console.log(newStuNew);
+
