8494ed1bbdc13740af6432676f3d2a60f59b3e19
jwilson23 <jvrahim23@yahoo.com>
2023-11-12 20:22:46 +0100

    Updated Assignment Two homework

---
+++ Week 1/Day 4/nov12.js
@@ -0,0 +1,24 @@
+alert("Welcome");
+console.log('2+2');
+console.log(2+2);
+
+const fistStu='Peter'
+
+let newStuNew='Lucas'
+newStuNew=newStuNew+' '+'Peter'
+console.log(newStuNew);
+newStuNew='Jack';
+console.log(newStuNew);
+
+const clientOne='Lucas';
+const clientOnePetName='Rex';
+const clientOnePetSpecies='Dog';
+const clientOnePetAge=2;
+
+const summaryInfo=clientOne+'\'s'+clientOnePetName+'is a '+clientOnePetSpecies+' and is '+clientOnePetAge+'.'
+console.log(summaryInfo)
+console.log(`${client}'s ${clientOnePetName} is a ${clientOnePetAge} years old ${clientOnePetSpecies}`)
+
+const calRetirement=()=>{console.log('hello three.')}
+
+calRetirement();
